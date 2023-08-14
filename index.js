877712fae31776543af4e28ee2678c65377cd464
mahidesi55 <msraikaji0055@gmail.com>
2023-08-14 14:04:03 +0530

    firstcommit

--- index.js
+++ index.js
@@ -1,75 +1,81 @@
-// document.body.append(document.createElement("h1").innerHTML = "Quiz")
+// document.body.append(document.createElement("h1").innerHTML = "Quiz"
+const questions = [
+  {
+    question: "With which song did Lata Mangeshkar do her first Hindi playback ?",
+    options: ["Natai Chaitrachi Navalai", "Pa Lagoon Kar Jori Re Shyam", "Janani Janam Bhoomi", "Main Khili khili Phulvari"],
+    answer: "Pa Lagoon Kar Jori Re Shyam"
+  },
+  {
+    question: "Dr. A.P.J. Abdul Kalam used to play:",
+    options: ["Nadeswaram", "Veena", "Sitar", "Violin"],
+    answer: "Veena"
+  },
+  {
+    question: "The opening theme music of doordarshan was composed by :",
+    options: ["Pt. Omkarnath Thakur", "Ud. Amjad Ali Khan", "Pt. Ravi Shankar", "Ud. Bismillah Khan"],
+    answer: "Ud. Amjad Ali Khan"
+  },
+  {
+    question: "The Violinist was equally popular in Hindustani and Karnatic style :",
+    options: ["Lalgudi G. Jayraman", "Dr. N.S. Gopalakrishnan", "A. Narmada", "N. Ramani"],
+    answer: "Lalgudi G. Jayraman"
+  },
+];
 
-let maindiv = document.querySelector('.wapper2')
-let musicdata = document.querySelector('.box211')
-let artdata = document.querySelector('.box212')
-let codingdata = document.querySelector('.box213')
-let startbutton = document.querySelector('.box21 .start')
-let questionsdata = document.querySelector('.wapper3') 
+const maindiv = document.querySelector('.wapper2')
+const musicdata = document.querySelector('.box211')
+// const artdata = document.querySelector('.box212')
+// const codingdata = document.querySelector('.box213')
+const startbutton = document.querySelector('.box21 .start')
+const questionElement = document.querySelector('.question');
+const optionElements = document.querySelectorAll('label');
+const nextButton = document.querySelector('.nextButton');
+const quitButton = document.querySelector('.quitButton');
+const scoreElement = document.querySelector('.score');
 
-let question1 = document.querySelector('.wapper3 .questions1')
-let nextstep = document.querySelector('.container2 .page3a1')
-let quit = document.querySelector('.container2 .page3a2')
-
-let question2 = document.querySelector('.wapper3 .questions2') 
-let nextstep2 = document.querySelector('.container3 .page3a1')
-let quit2 = document.querySelector('.container3 .page3a2')
-
-let question3 = document.querySelector('.wapper3 .questions3')
-let nextstep3 = document.querySelector('.container4 .page3a1')
-let quit3 = document.querySelector('.container4 .page3a2')
-
-let question4 = document.querySelector('.wapper3 .questions4')
-let nextstep4 = document.querySelector('.container5 .page3a1')
-let quit4 = document.querySelector('.container5 .page3a2')
-
-
+let currentQuestionIndex = 0;
+let score = 0;
 
 musicdata.onclick =(e)=>{
-    e.preventDefault()
-    musicdata.style.border = '4px solid black'
-    // startbutton.style.disabled = 'false'
-    startbutton.removeAttribute('disabled')
+  e.preventDefault()
+  if(artdata.classList.contains('blackborder')){
+    artdata.classList.remove('blackborder');
+    musicdata.classList.add('blackborder')
+  }
+  else if(codingdata.classList.contains("blackborder")){
+    codingdata.classList.remove("blackborder")
+    musicdata.classList.add('blackborder')
+  }
+  else{
+    musicdata.classList.add('blackborder')
+  }
+ startbutton.removeAttribute('disabled')
+  
 }
-
-artdata.onclick = (e) => {
-   e.preventDefault()
-   artdata.style.border = '4px solid black'
-   startbutton.removeAttribute('disabled')
-}
-
-codingdata.onclick = (e) => {
-   e.preventDefault()
-   codingdata.style.border = '4px solid black'
-   startbutton.removeAttribute('disabled')
-}
-
-
-
 startbutton.onclick =(e)=>{
-    e.preventDefault()
-    questionsdata.style.display ='block'
-    maindiv.style.display='none'
+  e.preventDefault()
+  maindiv.style.display='none'
+  console.log(e)
+  questionElement.style.display ='block'
 
 }
-
 nextstep.onclick=(e)=>{
-   e.preventDefault()
-   question1.style.display = 'none'
-   question2.style.display = 'block'
+  e.preventDefault()
+  optionElements.style.display = 'none'
+  optionElements.style.display = 'block'
 } 
 nextstep2.onclick=(e)=>{
-    e.preventDefault()
-    question2.style.display = 'none'
-    question3.style.display = 'block'
- } 
- nextstep3.onclick=(e)=>{
-    e.preventDefault()
-    question3.style.display = 'none'
-    question4.style.display = 'block'
- }
- nextstep4.onclick=(e)=>{
-    e.preventDefault()
-    question4.style.display = 'none'
-    question5.style.display = 'block'
- }
+   e.preventDefault()
+   optionElements.style.display = 'none'
+   optionElements.style.display = 'block'
+} 
+nextstep3.onclick=(e)=>{
+   e.preventDefault()
+   optionElements.style.display = 'none'
+   optionElements.style.display = 'block'
+}
+nextstep4.onclick=(e)=>{
+   e.preventDefault()
+   optionElements.style.display = 'none'
+   optionElements.style.display = 'block'
+}
